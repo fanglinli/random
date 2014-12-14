ba48f3eb7983cbd0c4085d3918d1e7fa08ec3e7e
Fang Lin Li <fanglifll@gmail.com>
2014-12-13 22:47:22 -0500

    Update bubbleSort.cc

--- c++/bubbleSort.cc
+++ c++/bubbleSort.cc
@@ -10,7 +10,7 @@ template <typename T> void bubbleSort(std::vector<T>& v) {
 		changedHuh = false;
 
 		for (typename std::vector<T>::iterator j = v.begin() - 1; j != v.end(); ++j) {
-			if (*i < *(j+1)) {			// Checks if current is larger than next
+			if (*i < *(j+1)) {				// Checks if current is larger than next
 				temp = *i;				// If so, swap
 				*i = *(j+1);
 				*(j+1) = temp;
